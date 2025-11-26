a1e3850ff4eb472c748f6f2ca3befe3ab0d73203
astrocc333 <astrocc333@gmail.com>
2025-11-26 18:08:08 -0500

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("this is the project version 1");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
 //done with version one
