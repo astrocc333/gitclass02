e6a87d1dcdfc4077fe59576229d3ebb3054e9fdc
astrocc333 <astrocc333@gmail.com>
2025-12-02 22:43:39 -0500

    fourth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,8 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("this is the project version 1");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+
+        System.out.println("this is the project version 4");
+
     }
 }
-//done with version one
+//done with version four
