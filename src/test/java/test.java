82917817644e42f50d78d68c57db6622720ecd66
astrocc333 <astrocc333@gmail.com>
2025-12-04 09:47:37 -0500

    person2commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,8 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("this is the project version 4");
+        System.out.println("this is the project version 6 made by person 2");
 
     }
 }
-//done with version four
+//done with version six
